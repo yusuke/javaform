5a53b2bb846601faae2d364d890b54ddbe73991b
Yusuke Yamamoto <yusuke@mac.com>
2021-11-22 09:53:41 +0900

    消費税率を10%に

--- src/main/java/dynamicform/Form.java
+++ src/main/java/dynamicform/Form.java
@@ -94,7 +94,7 @@ public final class Form {
         items.add(item);
     }
 
-    final int 消費税率 = 8;
+    final int 消費税率 = 10;
 
     public void saveAs(OutputStream outputStream, String templateForm, String... excludes) throws IOException, DocumentException {
         stamper = new Stamper(Form.class.getResourceAsStream("/" + templateForm), outputStream);
